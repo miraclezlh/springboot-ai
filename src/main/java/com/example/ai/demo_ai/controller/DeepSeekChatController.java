eede3cf3a6e05dd9106a7af880859ce3371b4f77
season <706350462@qq.com>
2025-10-03 21:42:09 +0800

    SpringAI集成DeepSeek

---
+++ src/main/java/com/example/ai/demo_ai/controller/DeepSeekChatController.java
@@ -0,0 +1,40 @@
+package com.example.ai.demo_ai.controller;
+
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+/**
+ * @author 70635
+ */
+@RestController
+public class DeepSeekChatController {
+
+    private final OpenAiChatModel chatModel;
+
+    @Autowired
+    public DeepSeekChatController(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @PostMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", this.chatModel.call(message));
+    }
+
+    @PostMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return this.chatModel.stream(prompt);
+    }
+
+
+}
---
+++ src/main/java/com/example/ai/demo_ai/controller/MyController.java
@@ -0,0 +1,27 @@
+package com.example.ai.demo_ai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 70635
+ */
+//@RestController
+public class MyController {
+
+//    private final ChatClient chatClient;
+//
+//    public MyController(ChatClient.Builder chatClientBuilder) {
+//        this.chatClient = chatClientBuilder.build();
+//    }
+//
+//    @PostMapping("/ai")
+//    String generation(String userInput) {
+//        return this.chatClient.prompt()
+//                .user(userInput)
+//                .call()
+//                .content();
+//    }
+
+}
